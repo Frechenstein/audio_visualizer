713cccd0e450601b97caaf2f50faef6873efcd13
st187228 <st187228@stud.uni-stuttgart.de>
2025-04-06 15:41:09 +0200

    maven project setup

---
+++ src/test/java/de/audio_vis_theeJ/audio_visualizer/AppTest.java
@@ -0,0 +1,19 @@
+package de.audio_vis_theeJ.audio_visualizer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
